5f6b5280255b4833bba8ea574c6c3ce595817777
Nouhaila Moukaddime <nouhailamoukaddime@gmail.com>
2024-06-14 14:42:06 +0200

    Added firebase config file

---
+++ firebase-config.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAvLx9UYJPwikU4N5A66lIOjhJQEH_ozN0",
+  authDomain: "reddit-wannabe.firebaseapp.com",
+  projectId: "reddit-wannabe",
+  storageBucket: "reddit-wannabe.appspot.com",
+  messagingSenderId: "883377091503",
+  appId: "1:883377091503:web:8ed0d86259bb43b9f1755a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
